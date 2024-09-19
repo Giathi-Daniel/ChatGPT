29997854f78885e13b368e5b54585062af96688c
Daniel Giathi <blacksharkchi@proton.me>
2024-09-19 13:41:24 +0300

    create authentication context

--- chatgpt-react/src/context/AuthContext.js
+++ chatgpt-react/src/context/AuthContext.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import { createContext, useContext, useEffect, useState} from 'react'
+import { auth, provider } from '../firebase/config'
+import { signInWithPopup, signOut } from 'firebase/auth'
 
-const AuthContext = () => {
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const loginWihGoogle = () => signInWithPopup(auth, provider)
+  const logout = () => signOut(auth)
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+      setLoading(false)
+    })
+    return unsubscribe;
+  }, [])
+
+  const value = {
+    currentUser,
+    loginWihGoogle,
+    logout
+  }
+
   return (
-    <div>AuthContext</div>
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
   )
 }
+
+
 
 export default AuthContext
